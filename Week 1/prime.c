dda7e77d2d762584829b12e9a52ae321137cbd92
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2023-03-30 21:54:05 +0600

    Update prime.c

--- Week 1/prime.c
+++ Week 1/prime.c
@@ -1,3 +1,9 @@
+// Background
+// Prime numbers are defined as whole numbers greater than 1, 
+// whose only factors are 1 and itself. So 3 is prime because its only factors are 1 and 3, 
+// while 4 is composite and not prime, because it is the product of 2 × 2. 
+// In this lab you will write an algorithm to generate all prime numbers in a range specified by the user.
+
 #include <cs50.h>
 #include <stdio.h>
 

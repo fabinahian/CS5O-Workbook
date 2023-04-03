73e6cd5aeb49072ff36dca592bcbdf651b542d7d
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2023-04-03 19:49:40 +0600

    Update credit_card.c

--- Week 1/credit_card.c
+++ Week 1/credit_card.c
@@ -1,3 +1,10 @@
+// Most cards use an algorithm invented by Hans Peter Luhn of IBM. 
+// According to Luhn’s algorithm, you can determine if a credit card number is (syntactically) valid as follows:
+
+// Multiply every other digit by 2 starting with the number’s second-to-last digit, 
+// and then add those PRODUCTS' DIGITS together. Add the sum to the sum of the digits that weren’t multiplied by 2.
+// If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+
 #include <cs50.h>
 #include <stdio.h>
 
